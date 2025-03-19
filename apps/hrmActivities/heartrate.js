f02e0a2b03c69eb2a7619b7cccc4e2c69972cf20
jibe-b <user701@orange.fr>
2025-03-19 19:02:58 +0100

    better clean hractivities after leaving clock mode

--- apps/hrmActivities/heartrate.js
+++ apps/hrmActivities/heartrate.js
@@ -49,28 +49,12 @@ var lastHrmPt = []; // last xy coords we draw a line to
 
 var hrHistory = [];
 
+g.setColor(g.theme.fg);
+g.reset().setFont("6x8", 2).setFontAlign(0, -1);
+
 function onHRM(h) {
-  if (counter !== undefined) {
-    // the first time we're called remove
-    // the countdown
-    counter = undefined;
-    g.clearRect(0, 24, g.getWidth(), g.getHeight());
-  }
   hrmInfo = h;
-  /* On 2v09 and earlier firmwares the only solution for realtime
-  HRM was to look at the 'raw' array that got reported. If you timed
-  it right you could grab the data pretty much as soon as it was written.
-  In new firmwares, '.raw' is not available. */
-  /*
-  if (hrmInterval) clearInterval(hrmInterval);
-  hrmInterval = undefined;
-  if (hrmInfo.raw) {
-    hrmOffset = 0;
-    setTimeout(function () {
-      hrmInterval = setInterval(readHRM, 41);
-    }, 40);
-  }
-  */
+
   updateHrm();
 }
 
@@ -126,9 +110,7 @@ function updateHrm() {
 
 var rawMax = 0;
 var scale = 2000;
-//var MID = (g.getHeight()+80)/2;
-/* On newer (2v10) firmwares we can subscribe to get
-HRM events as they happen */
+
 Bangle.on('HRM-raw', function (heartRate) {
   hrmOffset++;
   if (hrmOffset > g.getWidth()) {
@@ -155,15 +137,7 @@ Bangle.on('HRM-raw', function (heartRate) {
   }
 });
 
-// It takes 5 secs for us to get the first HRM event
-var counter = 5;
-function countDown() {
-  if (counter) {
-    counter -= 1
-    //g.drawString(counter--,g.getWidth()/2,g.getHeight()/2, true);
-    setTimeout(countDown, 1000);
-  }
-}
+
 g.clear();
 
 Bangle.setUI({
@@ -181,33 +155,3 @@ Bangle.setUI({
 
 Bangle.loadWidgets();
 Bangle.drawWidgets();
-g.setColor(g.theme.fg);
-g.reset().setFont("6x8", 2).setFontAlign(0, -1);
-//g.drawString(/*LANG*/"Please wait...",g.getWidth()/2,g.getHeight()/2 - 16);
-countDown();
-
-
-//var wasHigh = 0, wasLow = 0;
-//var lastHigh = getTime();
-//var hrmList = [];
-
-/*
-var hrmInfo;
-
-function readHRM() {
-  if (!hrmInfo) return;
-
-  if (hrmOffset == 0) {
-    g.clearRect(0, 24, g.getWidth(), g.getHeight());
-    lastHrmPt = [-100, 0];
-  }
-  for (var i = 0; i < 2; i++) {
-    var a = hrmInfo.raw[hrmOffset];
-    hrmOffset++;
-    let y = E.clip(170 - (a * 2), 100, 230);
-    //g.setColor(g.theme.fg).drawLine(lastHrmPt[0],lastHrmPt[1],hrmOffset, y);
-    lastHrmPt = [hrmOffset, y];
-  }
-}
-
-*/
