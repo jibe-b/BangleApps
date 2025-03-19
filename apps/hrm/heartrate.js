1b782ab19d9c3c4594a40b84cd08fd4a19d9301d
jibe-b <user701@orange.fr>
2025-03-19 18:32:24 +0100

    the hrm app is back, with default code

--- apps/hrm/heartrate.js
+++ apps/hrm/heartrate.js
@@ -4,124 +4,55 @@ if (process.env.HWVERSION == 1) {
 }
 
 Bangle.setHRMPower(1);
-
-
-var sum = function (array) {
-  var total = 0;
-  for (var i = 0; i < array.length; i++) {
-    total += array[i];
-  }
-  return total;
-};
-
-var mean = (array) => {
-  return sum(array) / array.length;
-};
-
-var min = (array) => {
-  var sorted_array = array.sort((a, b) => a - b)
-  return sorted_array[0]
-}
-
-var max = (array) => {
-  var sorted_array = array.sort((a, b) => b - a)
-  return sorted_array[0]
-}
-
-/*
-var median = (array) => {
-  array = array.sort();
-  if (array.length % 2 === 0) { // array with even number elements
-    return (array[array.length/2] + array[(array.length / 2) - 1]) / 2;
-  }
-  else {
-    return array[(array.length - 1) / 2]; // array with odd number elements
-  }
-};
-*/
-
-
-var hrmInfo = {}
-var hrmOffset = 0;
+var hrmInfo = {}, hrmOffset = 0;
 var hrmInterval;
-var btm = g.getHeight() - 1;
+var btm = g.getHeight()-1;
 var lastHrmPt = []; // last xy coords we draw a line to
 
-var hrHistory = [];
-
 function onHRM(h) {
-  if (counter !== undefined) {
+  if (counter!==undefined) {
     // the first time we're called remove
     // the countdown
     counter = undefined;
-    g.clearRect(0, 24, g.getWidth(), g.getHeight());
+    g.clearRect(0,24,g.getWidth(),g.getHeight());
   }
   hrmInfo = h;
   /* On 2v09 and earlier firmwares the only solution for realtime
   HRM was to look at the 'raw' array that got reported. If you timed
   it right you could grab the data pretty much as soon as it was written.
   In new firmwares, '.raw' is not available. */
-  /*
   if (hrmInterval) clearInterval(hrmInterval);
   hrmInterval = undefined;
   if (hrmInfo.raw) {
     hrmOffset = 0;
-    setTimeout(function () {
-      hrmInterval = setInterval(readHRM, 41);
+    setTimeout(function() {
+      hrmInterval = setInterval(readHRM,41);
     }, 40);
   }
-  */
   updateHrm();
 }
-
 Bangle.on('HRM', onHRM);
 
-function updateHrm() {
+function updateHrm(){
+  var px = g.getWidth()/2;
+  g.setFontAlign(0,-1);
+  g.clearRect(0,24,g.getWidth(),80);
+  g.setFont("6x8").drawString(/*LANG*/"Confidence "+(hrmInfo.confidence || "--")+"%", px, 70);
 
-  if (hrmInfo.bpm) {
-    hrHistory = hrHistory.concat([hrmInfo.bpm])
-  }
-  
-  
-  var maxHistoryLength = 60 / 2
-  if (hrHistory.length > maxHistoryLength){
-    hrHistory = hrHistory.splice(-maxHistoryLength)
-  }
+  updateScale();
 
-  var px = g.getWidth() / 2;
-  g.setFontAlign(0, -1);
-  g.clearRect(0, 24, g.getWidth(), g.getHeight());
-  //g.setFont("6x8").drawString(/*LANG*/"Confidence "+(hrmInfo.confidence || "--")+"%", px, 70);
+  g.setFontAlign(0,0);
+  var str = hrmInfo.bpm || "--";
+  g.setFontVector(40).setColor(hrmInfo.confidence > 50 ? g.theme.fg : "#888").drawString(str,px,45);
+  px += g.stringWidth(str)/2;
+  g.setFont("6x8").setColor(g.theme.fg);
+  g.drawString(/*LANG*/"BPM",px+15,45);
+}
 
-  g.setFontAlign(0, 0);
-  var str = hrmInfo.bpm || "";
-  var fontSize = 60;
-  var py = g.getHeight() / 2;
-
-  g.setFontVector(fontSize)
-    .setColor(hrmInfo.confidence > 50 ? g.theme.fg : "#888")
-    .drawString(str, px, py);
-
-  px += g.stringWidth(str) / 2;
-  //py += g.stringHeight(str)/2;
-
-  g.setFont("6x8")
-    .setColor(g.theme.fg);
-
-  g.drawString(/*LANG*/"BPM", px + 25, py);
-
-  if (hrHistory.length > 0) {
-    var meanHrHistory = Math.round(mean(hrHistory)) // Math.trunc(
-    var minHrHistory = min(hrHistory)
-    var maxHrHistory = max(hrHistory)
-
-    g.setFontVector(15)
-    g.drawString(meanHrHistory.toString(), g.getWidth() / 2 - 30, g.getHeight() - 30)
-
-    g.drawString(maxHrHistory.toString(), g.getWidth() / 2 - 60, g.getHeight() - 40)
-    g.drawString(minHrHistory.toString(), g.getWidth() / 2 - 60, g.getHeight() - 20)
-    
-  }
+function updateScale(){
+    g.setFontAlign(-1,-1);
+    g.clearRect(2,70,40,78);
+    g.setFont("6x8").drawString(scale, 2, 70);
 }
 
 var rawMax = 0;
@@ -129,28 +60,29 @@ var scale = 2000;
 //var MID = (g.getHeight()+80)/2;
 /* On newer (2v10) firmwares we can subscribe to get
 HRM events as they happen */
-Bangle.on('HRM-raw', function (v) {
+Bangle.on('HRM-raw', function(v) {
   hrmOffset++;
-  if (hrmOffset > g.getWidth()) {
+  if (hrmOffset>g.getWidth()) {
     let thousands = Math.round(rawMax / 1000) * 1000;
     if (thousands > scale) scale = thousands;
 
-    g.clearRect(0, 24, g.getWidth(), g.getHeight());
+    g.clearRect(0,80,g.getWidth(),g.getHeight());
+    updateScale();
 
-    hrmOffset = 0;
-    lastHrmPt = [-100, 0];
+    hrmOffset=0;
+    lastHrmPt = [-100,0];
   }
   if (rawMax < v.raw) {
     rawMax = v.raw;
   }
-  let y = E.clip(btm - (8 + v.filt / 3000), btm - 24, btm);
-  //g.setColor(1,0,0).fillRect(hrmOffset,btm, hrmOffset, y);
-  y = E.clip(btm - (v.raw / scale * 84), 84, btm);
-  //g.setColor(g.theme.fg).drawLine(lastHrmPt[0],lastHrmPt[1],hrmOffset, y);
+  let y = E.clip(btm-(8+v.filt/3000),btm-24,btm);
+  g.setColor(1,0,0).fillRect(hrmOffset,btm, hrmOffset, y);
+  y = E.clip(btm - (v.raw/scale*84),84,btm);
+  g.setColor(g.theme.fg).drawLine(lastHrmPt[0],lastHrmPt[1],hrmOffset, y);
   lastHrmPt = [hrmOffset, y];
-  if (counter !== undefined) {
+  if (counter !==undefined) {
     counter = undefined;
-    g.clearRect(0, 24, g.getWidth(), g.getHeight());
+    g.clearRect(0,24,g.getWidth(),g.getHeight());
     updateHrm();
   }
 });
@@ -159,8 +91,7 @@ Bangle.on('HRM-raw', function (v) {
 var counter = 5;
 function countDown() {
   if (counter) {
-    counter -= 1
-    //g.drawString(counter--,g.getWidth()/2,g.getHeight()/2, true);
+    g.drawString(counter--,g.getWidth()/2,g.getHeight()/2, true);
     setTimeout(countDown, 1000);
   }
 }
@@ -168,8 +99,8 @@ g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
 g.setColor(g.theme.fg);
-g.reset().setFont("6x8", 2).setFontAlign(0, -1);
-//g.drawString(/*LANG*/"Please wait...",g.getWidth()/2,g.getHeight()/2 - 16);
+g.reset().setFont("6x8",2).setFontAlign(0,-1);
+g.drawString(/*LANG*/"Please wait...",g.getWidth()/2,g.getHeight()/2 - 16);
 countDown();
 
 
@@ -181,15 +112,15 @@ var hrmInfo;
 function readHRM() {
   if (!hrmInfo) return;
 
-  if (hrmOffset == 0) {
-    g.clearRect(0, 24, g.getWidth(), g.getHeight());
-    lastHrmPt = [-100, 0];
+  if (hrmOffset==0) {
+    g.clearRect(0,100,g.getWidth(),g.getHeight());
+    lastHrmPt = [-100,0];
   }
-  for (var i = 0; i < 2; i++) {
+  for (var i=0;i<2;i++) {
     var a = hrmInfo.raw[hrmOffset];
     hrmOffset++;
-    let y = E.clip(170 - (a * 2), 100, 230);
-    //g.setColor(g.theme.fg).drawLine(lastHrmPt[0],lastHrmPt[1],hrmOffset, y);
+    let y = E.clip(170 - (a*2),100,230);
+    g.setColor(g.theme.fg).drawLine(lastHrmPt[0],lastHrmPt[1],hrmOffset, y);
     lastHrmPt = [hrmOffset, y];
   }
 }
